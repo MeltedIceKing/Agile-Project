4ff2ca5f3669f870a4725f1786e40b5fc3396cf3
Bryan Rainbow <bryanrbow@gmail.com>
2021-05-04 22:54:29 -0700

    Add test for invalid route and create route

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -4,9 +4,25 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 
-describe('Verify each GET route', () => {
+describe('Test each GET route', () => {
 
-    // First test - /codeman route
+    // First test - invalid route
+    it("Test GET invalid route", async () => {
+
+        const res = await request.get("/thisroutedoesnotexist");
+
+        expect(res.status).toBe(404);
+    })
+
+    // Second test - / route
+    it("Test GET / route", async () => {
+
+        const res = await request.get("/");
+
+        expect(res.status).toBe(200);
+    })
+
+    // Third test - /codeman route
     it("Test GET /codeman route", async () => {
 
         const res = await request.get("/codeman");
@@ -14,10 +30,10 @@ describe('Verify each GET route', () => {
         expect(res.status).toBe(200);
     })
 
-    // Second test - / route
-    it("Test GET / route", async () => {
+    // Fourth test - /create route
+    it("Test GET /create route", async () => {
 
-        const res = await request.get("/");
+        const res = await request.get("/create");
 
         expect(res.status).toBe(200);
     })

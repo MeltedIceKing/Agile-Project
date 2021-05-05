81cc1b64147c5fc122d977336bad360e9d983b3c
MeltedIceKing <shilsingh01@yahoo.com>
2021-05-04 19:12:42 -0700

    Update start.js

--- start.js
+++ start.js
@@ -2,6 +2,6 @@ const app = require('./index.js');
 
 app.listen(3001, function () {
     console.log(
-        "Server running. Visist localhost:3001/codeman in your browser"
+        "Server running. Visist localhost:3001 in your browser"
     );
 });

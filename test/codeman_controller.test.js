fe10286e9f8dae8b19fb27060be7942915adc44a
Bryan Rainbow <bryanrbow@gmail.com>
2021-05-19 18:38:12 -0700

    Add tests for codeman_controller.created

--- test/codeman_controller.test.js
+++ test/codeman_controller.test.js
@@ -84,5 +84,114 @@ describe('Test codeman controller', () => {
             expect(res.redirect).toHaveBeenCalled();
             expect(res.redirect).toHaveBeenCalledWith('/welcome');
         })
+
+        it("Test content of req.user after clicking save with 1 of each (file, class, property, and method)", () => {
+            req.body = {
+                "file-name-0": ["test file 1", "test file desc 1"],
+                "class-name-0": ["test class 1", "test class desc 1"],
+                "property-name-0": ["test property 1", "private-property"],
+                "method-name-0": ["test method 1", "str", "int, int", "test def"]
+            }
+            req.user = { projects: [] }
+            codeman_controller.created(req, res);
+
+            expect(req.user.projects).toHaveLength(1);
+
+            projectObj = req.user.projects[0]
+            console.log(projectObj);
+            console.log(projectObj.files);
+            console.log(projectObj.files[0].classes)
+            console.log(projectObj.files[0].classes[0].methods)
+            console.log(projectObj.files[0].classes[0].props)
+
+            // Test project name and id
+            expect(projectObj.name).toEqual("Project 1");
+            expect(projectObj.id).toBe(1);
+
+            // Test project file list
+            expect(projectObj.files).toHaveLength(1);
+            expect(projectObj.files[0].name).toEqual("test file 1");
+            expect(projectObj.files[0].desc).toEqual("test file desc 1");
+
+
+            // Test project classes of the first (only) file
+            projectClasses = projectObj.files[0].classes
+
+            expect(projectClasses).toHaveLength(1);
+            expect(projectClasses[0].name).toEqual("test class 1");
+            expect(projectClasses[0].desc).toEqual("test class desc 1");
+
+            // Test methods of first (only) class
+            expect(projectClasses[0].methods).toHaveLength(1);
+            expect(projectClasses[0].methods[0].name).toEqual("test method 1");
+            expect(projectClasses[0].methods[0].rety).toEqual("str");
+            expect(projectClasses[0].methods[0].args).toEqual("int, int");
+            expect(projectClasses[0].methods[0].desc).toEqual("test def");
+
+            // Test properties of first (only) class
+            expect(projectClasses[0].props).toHaveLength(1);
+            expect(projectClasses[0].props[0].name).toEqual("test property 1");
+            expect(projectClasses[0].props[0].type).toEqual("private-property");
+        })
+
+        it("Test content of req.user after clicking save with unequal amounts of data", () => {
+            req.body = {
+                "file-name-0": ["test file 1", "test file desc 1"],
+                "file-name-1": ["test file 2", "test file desc 2"],
+                "class-name-0": ["test class 1", "test class desc 1"],
+                "property-name-0": ["test property 1", "private-property"],
+                "method-name-0": ["test method 1", "str", "int, int", "test def"],
+                "file-name-2": ["test file 3", "test file desc 3"],
+                "class-name-1": ["test class 2", "test class desc 2"],
+                "class-name-2": ["test class 3", "test class desc 3"]
+            }
+            req.user = { projects: [] }
+            codeman_controller.created(req, res);
+
+            expect(req.user.projects).toHaveLength(1);
+
+            projectObj = req.user.projects[0]
+
+            // Test each of the files
+            expect(projectObj.files).toHaveLength(3);
+
+            expect(projectObj.files[0].name).toEqual("test file 1");
+            expect(projectObj.files[0].desc).toEqual("test file desc 1");
+            expect(projectObj.files[0].classes).toHaveLength(0);
+
+            expect(projectObj.files[1].name).toEqual("test file 2");
+            expect(projectObj.files[1].desc).toEqual("test file desc 2");
+            expect(projectObj.files[1].classes).toHaveLength(1);
+
+            expect(projectObj.files[2].name).toEqual("test file 3");
+            expect(projectObj.files[2].desc).toEqual("test file desc 3");
+            expect(projectObj.files[2].classes).toHaveLength(2);
+
+
+            // Test first class object and sub-objects
+            projectClasses = projectObj.files[1].classes
+            expect(projectClasses[0].name).toEqual("test class 1");
+            expect(projectClasses[0].desc).toEqual("test class desc 1");
+            expect(projectClasses[0].methods).toHaveLength(1);
+            expect(projectClasses[0].methods[0].name).toEqual("test method 1");
+            expect(projectClasses[0].methods[0].rety).toEqual("str");
+            expect(projectClasses[0].methods[0].args).toEqual("int, int");
+            expect(projectClasses[0].methods[0].desc).toEqual("test def");
+            expect(projectClasses[0].props).toHaveLength(1);
+            expect(projectClasses[0].props[0].name).toEqual("test property 1");
+            expect(projectClasses[0].props[0].type).toEqual("private-property");
+
+            // Test last two classes from the last file object
+            projectClasses = projectObj.files[2].classes
+            expect(projectClasses[0].name).toEqual("test class 2");
+            expect(projectClasses[0].desc).toEqual("test class desc 2");
+            expect(projectClasses[0].methods).toHaveLength(0);
+            expect(projectClasses[0].props).toHaveLength(0);
+
+            expect(projectClasses[1].name).toEqual("test class 3");
+            expect(projectClasses[1].desc).toEqual("test class desc 3");
+            expect(projectClasses[1].methods).toHaveLength(0);
+            expect(projectClasses[1].props).toHaveLength(0);
+        })
     });
 });
